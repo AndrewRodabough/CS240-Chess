80fec900a01fd4b7d62c978e4e47159ad6cc43c9
AndrewRodabough <arodabough@hotmail.com>
2024-02-25 21:35:02 -0700

    port is able to start (Phase 3)

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,21 +6,28 @@ public class Main {
     public static void main(String[] args) {
         try {
             int port = 6969;
+
+            // sets port if specified in command line
             if (args.length >= 1) {
                 port = Integer.parseInt(args[0]);
             }
 
-            var server = new Server().run(port);
+            Server server = new Server();
+            server.run(port);
             port = server.port();
+
             System.out.printf("Server started on port %d%n", port);
             return;
+
         } catch (Throwable ex) {
             System.out.printf("Unable to start server: %s%n", ex.getMessage());
         }
-        System.out.println("""
-                Pet Server:
+
+        System.out.println(
+                """
+                Server:
                 java ServerMain <port> [<dburl> <dbuser> <dbpassword> <dbname>]
-                """);
-    }
+                """
+        );
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import dataAccess.DataAccess;
 import server.websocket.WebSocketHandler;
 import service.Service;
+import server.ResponseException;
 
 import spark.*;
 
@@ -20,6 +21,8 @@ public class Server {
 
         Spark.staticFiles.location("web");
 
+        Spark.webSocket("/connect", webSocketHandler);
+
         // Register your endpoints and handle exceptions here.
         Spark.post("/chess", this::CreateGameHandler);
         Spark.post("/chess", this::LoginHandler);
@@ -28,6 +31,7 @@ public class Server {
         Spark.get("/chess", this::ListGamesHandler);
         Spark.delete("/chess", this::ClearApplicationHandler);
         Spark.delete("/chess", this::LogoutHandler);
+        //Spark.exception(ResponseException.class, this::exceptionHandler);
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -39,24 +43,28 @@ public class Server {
     public void stop() { Spark.stop(); Spark.awaitStop(); }
 
     private Object ClearApplicationHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object CreateGameHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object JoinGameHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object ListGamesHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object LoginHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object LogoutHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
     private Object RegistrationHandler(Request req, Response res) {
-
+        return "Not Implemented";
     }
+
+    /*
+    private void exceptionHandler(ResponseException ex, Request req, Response res) { res.status(ex.StatusCode()); }
+     */
 }
