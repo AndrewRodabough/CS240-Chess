34868527c9a9b48b56562a3424f7d8ed88429ce8
AndrewRodabough <arodabough@hotmail.com>
2024-02-27 19:09:51 -0700

    fixed unit tests (Phase 3)

--- server/src/test/java/servceTests/Logout.java
+++ server/src/test/java/servceTests/Logout.java
@@ -1,5 +1,7 @@
 package servceTests;
 
+import model.AuthData;
+import model.UserData;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Assertions.*;
@@ -7,11 +9,19 @@ import org.junit.jupiter.api.Assertions.*;
 public class Logout {
     @Test
     public void RunPositive() {
+        service.Clear.Run();
+        UserData user = new UserData("bob", "password", "bob@bob.com");
+        AuthData auth = service.Register.Run(user);
+        Assertions.assertTrue(service.Logout.Run(auth.authToken()));
 
     }
 
     @Test
     public void RunNegative() {
-
+        service.Clear.Run();
+        UserData user = new UserData("bob", "password", "bob@bob.com");
+        AuthData auth = service.Register.Run(user);
+        service.Logout.Run(auth.authToken());
+        Assertions.assertFalse(service.Logout.Run(auth.authToken()));
     }
 }
