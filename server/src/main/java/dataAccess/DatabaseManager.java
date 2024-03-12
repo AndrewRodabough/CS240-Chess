a90ab993fa7a2ac009402572fc388acc61dc3b48
AndrewRodabough <arodabough@hotmail.com>
2024-03-11 21:53:07 -0600

    fix? (phase 4)

--- server/src/main/java/dataAccess/DatabaseManager.java
+++ server/src/main/java/dataAccess/DatabaseManager.java
@@ -15,7 +15,7 @@ public class DatabaseManager {
      */
     static {
         try {
-            try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("resources/db.properties")) {
+            try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
                 if (propStream == null) throw new Exception("Unable to laod db.properties");
                 Properties props = new Properties();
                 props.load(propStream);
